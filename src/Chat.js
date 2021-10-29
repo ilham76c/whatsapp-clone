901b81668789f7590490b4e052fe4f018a26f68d
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-29 22:30:04 +0700

    setting router to switch chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,27 @@
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@mui/icons-material';
 import { Avatar, IconButton } from '@mui/material';
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './Chat.css';
+import db from './firebase';
 
 function Chat() {
     const [input, setInput] = useState('');
     const [seed, setSeed] = useState('');
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms').doc(roomId).onSnapshot((snapshot) => 
+                setRoomName(snapshot.data().name)
+            );
+        }
+    }, [roomId]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
-    }, []);
+    }, [roomId]);
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -23,7 +35,7 @@ function Chat() {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
                 
                 <div className="chat__headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at ...</p>
                 </div>
 

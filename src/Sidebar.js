32ad19ae8d32a6b0da6c8f7902168a9444288028
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-30 02:55:49 +0700

    show photo profile based on gmail photo profile

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from 'react'
 import './Sidebar.css';
 import SidebarChat from './SidebarChat';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
 
 function Sidebar() {
     const [rooms, setRooms] = useState([]);
-    
+    const [{ user }, dispatch] = useStateValue();
+
     useEffect(() => {
         db.collection('rooms').onSnapshot(snapshot => (
             setRooms(snapshot.docs.map(doc => (
@@ -22,7 +24,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLarge />

f3ec6719ae81da44b5a64c65792ed860d79d9359
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-31 01:31:05 +0700

    show messages and last seen based on room

--- src/Chat.js
+++ src/Chat.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './Chat.css';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
 
 function Chat() {
     const [input, setInput] = useState('');
@@ -11,6 +13,7 @@ function Chat() {
     const { roomId } = useParams();
     const [roomName, setRoomName] = useState('');
     const [messages, setMessages] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
         if (roomId) {
@@ -33,6 +36,11 @@ function Chat() {
     const sendMessage = (e) => {
         e.preventDefault();
         console.log('You type >>> ', input);
+        db.collection('rooms').doc(roomId).collection('messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
         setInput('');
     };
 
@@ -43,7 +51,14 @@ function Chat() {
                 
                 <div className="chat__headerInfo">
                     <h3>{roomName}</h3>
-                    <p>Last seen at ...</p>
+                    <p>
+                        Last seen {" "}
+                        {
+                            new Date(messages[messages.length - 1]?.
+                                timestamp?.toDate()
+                            ).toUTCString()
+                        }
+                    </p>
                 </div>
 
                 <div className="chat__headerRight">
@@ -61,13 +76,15 @@ function Chat() {
 
             <div className="chat__body">
                 {messages.map(message => (
-                    <div className={`chat__message ${true && "chat__receiver"}`}>
+                    <p 
+                        className={`chat__message ${message.name === user.displayName && "chat__receiver"}`}
+                    >
                         <span className="chat__name">{message.name}</span>
                             {message.message}
                         <span className="chat__timestamps">
                             {new Date(message.timestamp?.toDate()).toUTCString()}
                         </span>
-                    </div>
+                    </p>
                 ))}
             </div>
 

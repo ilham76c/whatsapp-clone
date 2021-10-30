6fab2b1620811760bfb3f4caf4ec6a7a95ddc1ee
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-31 01:55:59 +0700

    show sidebar last message from database

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -6,6 +6,16 @@ import './SidebarChat.css'
 
 function SidebarChat({ id, name, addNewChat }) {
     const [seed, setSeed] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        if (id) {
+            db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc')
+                .onSnapshot(snapshot => (
+                    setMessage(snapshot.docs.map((doc) => doc.data()))
+                ))
+        }
+    }, [id]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -28,7 +38,7 @@ function SidebarChat({ id, name, addNewChat }) {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
                 <div className="sidebarChat__info">
                     <h2>{ name }</h2>
-                    <p>Last message...</p>
+                    <p>{message[0]?.message}</p>
                 </div>
             </div>
         </Link>
